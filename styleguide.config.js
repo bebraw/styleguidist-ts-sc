a4877c45e5be38b5a7e84fd6bcf2365d3abc6484
Juho Vepsalainen <bebraw@gmail.com>
2018-09-17 14:28:37 +0200

    fix(styleguide): Use the fixed version of the parser

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
+    propsParser: require('@bebraw/react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
     components: 'src/components/**/*.{js,jsx,ts,tsx}',
     webpackConfig: require('./config/webpack.config.dev.js')
 };
