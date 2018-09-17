fc5f26b8cb66a652ac5743dbfb5bf50e2ba7fbef
Juho Vepsalainen <bebraw@gmail.com>
2018-09-17 11:51:23 +0200

    chore: Add /** @component */ annotations
    I suppose ideally Babel would do this.

--- src/components/ButtonSC.js
+++ src/components/ButtonSC.js
@@ -2,4 +2,5 @@ import styled from 'styled-components';
 
 const ButtonSC = styled.button``
 
+/** @component */
 export default ButtonSC
--- src/components/ButtonSCTS.tsx
+++ src/components/ButtonSCTS.tsx
@@ -2,4 +2,5 @@ import styled from 'styled-components';
 
 const ButtonSCTS = styled.button``
 
+/** @component */
 export default ButtonSCTS
