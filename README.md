2332f8185cecf830f9564865c33bafe869634c1c
Juho Vepsalainen <bebraw@gmail.com>
2018-09-17 11:01:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Styleguidist with TypeScript and styled-components
+
+1. `yarn install`
+2. `yarn run styleguide:watch`
+
---
+++ styleguide.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    components: 'src/components/**/*.js',
+    webpackConfig: require('./config/webpack.config.dev.js')
+};
