f681957bfe4c7f51b544081d75fde98adc0f23af
Juho Vepsalainen <bebraw@gmail.com>
2018-09-17 13:45:04 +0200

    chore: Demonstrate parser bug

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // propsParser: require('react-docgen-typescript').withDefaultConfig([]).parse,
+    propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
     components: 'src/components/**/*.{js,jsx,ts,tsx}',
     webpackConfig: require('./config/webpack.config.dev.js')
 };
