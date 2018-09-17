071dfc52a6d047a4ab06052061db623beb22939b
Juho Vepsalainen <bebraw@gmail.com>
2018-09-17 14:48:34 +0200

    chore: Use a custom version of TS parser only for TS files

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,13 @@
+const path = require('path')
+const reactDocgen = require('react-docgen')
+const tsDocgen = require('@bebraw/react-docgen-typescript').withCustomConfig('./tsconfig.json');
+
 module.exports = {
-    propsParser: require('@bebraw/react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
+    propsParser(filePath, source, resolver, handlers) {
+        const parser = path.extname(filePath).startsWith('.ts') ? tsDocgen : reactDocgen
+
+        return parser.parse(source, resolver, handlers)
+    },
     components: 'src/components/**/*.{js,jsx,ts,tsx}',
     webpackConfig: require('./config/webpack.config.dev.js')
 };
